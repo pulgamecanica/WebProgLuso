c309e40476cef3f782c6357f7be69319687d3307
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-02 05:05:48 +0100

    Created main layout, and few info

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  document.body.style.background = "#19191e";
+  document.getElementById('navBar').style.background = "black";
+  // document.getElementById('introduction').style.background = "gray";
+}
