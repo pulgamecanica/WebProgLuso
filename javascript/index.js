5048d56e64f6d52ee8f997f56cb6e1ad6238e365
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-02 16:46:15 +0100

    Added JS Collapsibles and worked on the contents

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,16 @@ function myFunction() {
   document.getElementById('navBar').style.background = "black";
   // document.getElementById('introduction').style.background = "gray";
 }
+function collapse(colName){
+	var coll = document.getElementsByClassName("collapsible-"+colName);
+	var i;
+	for (i = 0; i < coll.length; i++) {
+	    coll[i].classList.toggle("active-collapsible");
+	    var content = coll[i].nextElementSibling;
+	    if (content.style.display === "block") {
+	      content.style.display = "none";
+	    } else {
+	      content.style.display = "block";
+	    }
+	}
+}
