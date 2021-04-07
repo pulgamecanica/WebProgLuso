33788b257e3797307806cd1c37fb1748e8a9cd5f
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-07 22:42:43 +0100

    Finished the About page and created the 'layouts' folder, changed the websit-guide.html

---
+++ img/Silly Website - Layouts_files/index.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  document.body.style.background = "#19191e";
+  document.getElementById('navBar').style.background = "black";
+  document.getElementById('content-wrapperID').style.background = "rgba(22, 99, 67, 0.5)"
+  // document.getElementById('introduction').style.background = "gray";
+}
+function collapse(colName){
+	var coll = document.getElementsByClassName("collapsible-"+colName);
+	var i;
+	for (i = 0; i < coll.length; i++) {
+	    coll[i].classList.toggle("active-collapsible");
+	    var content = coll[i].nextElementSibling;
+	    if (content.style.display === "block") {
+	      content.style.display = "none";
+	    } else {
+	      content.style.display = "block";
+	    }
+	}
+}
+
+
