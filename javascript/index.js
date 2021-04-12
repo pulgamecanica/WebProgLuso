0d766a32d31679ed2e8c2a891752ad1c2d2c6e83
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-12 19:09:30 +0100

    Finished creating games with js and css

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,29 @@
+var colors = [5];
 var	logoClickGameCounter = 0;
+var colorGameGuessed = 0;
 var counter = 100;
 var timeOut;
-var colors = [5];
 var selectedColor;
-var colorGameGuessed = 0;
-
+var logoLoved = false;
+const messages = ["I hope you are bored...","What's your favourite boring thing to do?","Do you have a boring pet?","I like your smile.","Everything is also nothing :O .","I want you to be bored.","Share with your friends your heart.","Be kind.","Be bored.","Don't get creative, being bored is better.","Do you like being bored?","We love boring things.","Happy boring day!","How is it going?","I want a boring snack!","Ouch...","Hey! Stop pushing the button.","You are boring.","I am boring.","BORING MESSAGE","Are you hungry?","Do you like this website?","How many times have you pushed this button?","You done yet?","I don't like creative people...","Procrastination is boring!","Encourage others to be bored.","Boring is love.","Keep Clam and stay bored.","I love the Boring Web","Boring Web supports you.","We love you.","Get lost!","BOOOOooooorrrRRinG."];
+function loveTheLogo() {
+	if (logoLoved == true) {
+		return;
+	}else{
+		document.getElementById('love-the-logo-clicks').innerHTML = parseInt(document.getElementById('love-the-logo-clicks').innerHTML) + 1;
+		logoLoved = true;
+	}
+} 
 function loadColorGame() {
 	var optionFields = document.getElementsByClassName("colorOption");
 	document.getElementById('start-restart-color-game').innerHTML = '<i class="fas fa-redo-alt"></i>'; 
-	for(counter = 0; counter < optionFields.length; counter++) {
-		console.log(optionFields[counter]);
-		optionFields[counter].innerHTML = colors[counter] = getColor();
-		optionFields[counter].style.color = colors[counter];
+	for(var colorcounter = 0; colorcounter < optionFields.length; colorcounter++) {
+		optionFields[colorcounter].innerHTML = colors[colorcounter] = getColor();
+		optionFields[colorcounter].style.color = colors[colorcounter];
 	}
 	document.getElementById("guess-the-color-game").style.backgroundColor = (selectedColor = colors[Math.floor((Math.random()*5))]);
 }
-function colorGuess(option){
+function colorGuess(option) {
 	if (colors[option] == selectedColor) {
 		colorGameGuessed+= 1;
 		document.getElementById('colorGuessedCounter').innerHTML = colorGameGuessed;
@@ -95,17 +103,40 @@ function logoClickGame() {
 }
 function youClickYouWin() {
 	document.getElementById('you-cant-win-text').innerHTML = "<span id='youClickedYouWon'>You WON!</span>"; 
+	return "Nice, you won!";
 }
 function getHint(gameHint) {
 	if (gameHint == 'you-cant-win-game') {
-		alert("There is a mysterious function: logoClickGame(). I will say no more....");
+		alert("There is a mysterious function: youClickYouWin(). I will say no more....");
+	}else if (gameHint == 'dj-ninja-game') {
+		alert("If you can't spot the Ninja, try to change the game background color....");
 	}
 }
-
-
-
-
-
-
-
-
+function switcher(switchId) {
+	var switchElement = document.getElementById('switch' + switchId);
+	switchElement.style.animation =  'switch 1s ease';
+	setTimeout(function(){
+		switchElement.style.transform =  'translate3d(15px, 0, 0)';
+		switchElement.style.backgroundColor =  'green';
+	}, 1000);
+}
+function rgbaColor(colorCode) {
+	var backgroundElement = document.getElementById('color-rgba');
+	if (colorCode == 0) {
+		backgroundElement.style.background = 'red';
+		console.log("backgroundRED");
+	}else if (colorCode == 1) {
+		backgroundElement.style.background = 'green';
+		console.log("backgroundGREEN");
+	}else if (colorCode == 2) {
+		backgroundElement.style.background = 'blue';
+		console.log("backgroundBLUE");
+	}
+}
+function revealNinja() {
+	document.getElementById('ninja').style.color = 'white';
+	document.getElementById('ninja').style.display = 'block';
+}
+function getMessage() {
+	document.getElementById('mysterious-message-message').innerHTML = messages[Math.floor(Math.random()*messages.length)];
+}
