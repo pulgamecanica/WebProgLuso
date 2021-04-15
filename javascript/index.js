50dc37ceb196210c85fddce7ae0be8f46143a7d1
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-15 13:34:36 +0100

    Added the image window on Posts page

--- javascript/index.js
+++ javascript/index.js
@@ -213,3 +213,35 @@ function revealNinja() {
 function getMessage() {
 	document.getElementById('mysterious-message-message').innerHTML = messages[Math.floor(Math.random()*messages.length)];
 }
+function openImage(url) {
+	var windowDisplayer = document.getElementById('imageWindowDisplayer');
+	windowDisplayer.innerHTML = "";
+	var image = document.createElement('IMG');
+	var closeIcon = document.createElement('I');
+	closeIcon.classList.add("fas");
+	closeIcon.classList.add("fa-times-circle");
+	closeIcon.addEventListener("click", closeImage);
+	image.src = 'img/' + url;
+	windowDisplayer.appendChild(image);
+	windowDisplayer.appendChild(closeIcon);
+	windowDisplayer.style.display = "block";
+	document.onkeydown = function(evt) {
+    	if (evt.keyCode == 27) {
+        	closeImage();
+    	}
+	};	
+}
+function closeImage() {
+	document.getElementById('imageWindowDisplayer').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
